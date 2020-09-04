45114496000f3bde6f7eabff8781511c510ab795
Madan Narra <narra.madan@outlook.com>
2020-09-05 09:23:44 +1000

    Initial Commit

---
+++ src/main/java/com/toomuch2learn/reactive/nativecrud/catalogue/SpringReactiveNativeCatalogueCrudApplication.java
@@ -0,0 +1,13 @@
+package com.toomuch2learn.reactive.nativecrud.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveNativeCatalogueCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveNativeCatalogueCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toomuch2learn/reactive/nativecrud/catalogue/SpringReactiveNativeCatalogueCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toomuch2learn.reactive.nativecrud.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveNativeCatalogueCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
