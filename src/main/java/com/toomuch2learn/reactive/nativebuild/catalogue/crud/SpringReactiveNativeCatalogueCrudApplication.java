254437b672023fe03a9d9fbb98c75f42fd3dc436
Madan Narra <narra.madan@outlook.com>
2020-09-05 13:35:56 +1000

    Native build is created and started successfully. But failing for API requests

--- src/main/java/com/toomuch2learn/reactive/nativebuild/catalogue/crud/SpringReactiveNativeCatalogueCrudApplication.java
+++ src/main/java/com/toomuch2learn/reactive/nativebuild/catalogue/crud/SpringReactiveNativeCatalogueCrudApplication.java
@@ -3,7 +3,7 @@ package com.toomuch2learn.reactive.nativebuild.catalogue.crud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(proxyBeanMethods=false)
 public class SpringReactiveNativeCatalogueCrudApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/toomuch2learn/reactive/nativebuild/catalogue/crud/configuration/ApplicationConfiguration.java
+++ src/main/java/com/toomuch2learn/reactive/nativebuild/catalogue/crud/configuration/ApplicationConfiguration.java
@@ -2,6 +2,6 @@ package com.toomuch2learn.reactive.nativebuild.catalogue.crud.configuration;
 
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Configuration(proxyBeanMethods=false)
 public class ApplicationConfiguration {
 }
