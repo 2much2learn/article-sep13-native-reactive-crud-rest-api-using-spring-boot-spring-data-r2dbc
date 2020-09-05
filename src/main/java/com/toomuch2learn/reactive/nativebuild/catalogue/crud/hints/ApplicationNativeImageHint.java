9901024d2a8cc6b4abb90a1642c938cc2c3ab790
Madan Narra <narra.madan@outlook.com>
2020-09-05 14:29:59 +1000

    Fixed issue with jackson serialization due to nativeimagebuild. Fixed by adding hint class

---
+++ src/main/java/com/toomuch2learn/reactive/nativebuild/catalogue/crud/hints/ApplicationNativeImageHint.java
@@ -0,0 +1,20 @@
+package com.toomuch2learn.reactive.nativebuild.catalogue.crud.hints;
+
+import org.springframework.graalvm.extension.NativeImageConfiguration;
+import org.springframework.graalvm.extension.NativeImageHint;
+import org.springframework.graalvm.extension.TypeInfo;
+import org.springframework.graalvm.type.AccessBits;
+
+/**
+ * https://github.com/spring-projects-experimental/spring-graalvm-native/blob/master/spring-graalvm-native-feature/src/main/java/org/springframework/graalvm/extension/NativeImageHint.java
+ */
+
+@NativeImageHint(typeInfos = {
+    @TypeInfo(
+        typeNames = {
+            "com.toomuch2learn.reactive.nativebuild.catalogue.crud.model.CatalogueItem"
+        },
+        access= AccessBits.FULL_REFLECTION)
+})
+public class ApplicationNativeImageHint implements NativeImageConfiguration {
+}
